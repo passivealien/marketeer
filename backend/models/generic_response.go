efa1749d2edc698e3db565966be5a3b99400d27c
neil <neil.anonat@rafi.ph>
2022-11-25 12:55:10 +0800

    m

--- backend/models/generic_response.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type GenericResponse struct {
-	Code    int32 `json:"code"`
-	Status  string
-	Message string `json:"message,omitempty"`
-	//Data    interface{} `json:",omitempty"`
-}
